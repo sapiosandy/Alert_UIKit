cea48453c1cb8635bc66900901a00ed46f9ade42
Sandy Gomez <sgomez1029@gmail.com>
2024-02-24 18:06:41 -0800

    Creating an Alert using UIKit (USCD Extension)

--- Alert_UIKit/ViewController.swift
+++ Alert_UIKit/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mySwitch: UISwitch!
+    var whichStyle: UIAlertController.Style = .alert
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if mySwitch.isOn {
+            whichStyle = .alert
+        } else {
+            whichStyle = .actionSheet
+        }
+        
+        let alert = UIAlertController(title: "Warning", message: "Do you really want to delete this message?", preferredStyle: whichStyle)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: { action -> Void in
+            //Just dismiss the action sheet
+        })
+        alert.addAction(okAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
